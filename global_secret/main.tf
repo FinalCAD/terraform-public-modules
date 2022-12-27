0575db64396800259ebb6b829542b0838faf93b0
damienjacinto <damien.jacinto@gmail.com>
2022-12-27 10:08:56 +0100

    create global secret module

---
+++ global_secret/main.tf
@@ -0,0 +1,22 @@
+/**
+* This tiny module enables to read a secret stored in AWS secret manager and to convert it to a terraform map.
+*
+* If both, finalcad_environment and aws_region_friendly_name are empty it will use terraform/global as key prefix.
+* If cross_region is true, it will use terraform/finalcad_environment as key prefix
+* Otherwise it will use terraform/finalcad_environment/aws_region_friendly_name
+*/
+
+locals {
+  # we use the same structure as tfstate files
+  env_path    = var.finalcad_environment == "" ? "global" : "${var.finalcad_environment}/${local.region}"
+  region      = var.cross_region ? "global" : var.aws_region_friendly_name
+  secret_name = "terraform/${local.env_path}/${var.secret_shortname}"
+}
+
+data "aws_secretsmanager_secret" "secret" {
+  name = local.secret_name
+}
+
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secret[0].id
+}
---
+++ global_secret/outputs.tf
@@ -0,0 +1,9 @@
+output "secret" {
+  description = "All secrets as a map"
+  value       = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)
+}
+
+output "secret_id" {
+  description = "Secret id"
+  value       = data.aws_secretsmanager_secret.secret.id
+}
